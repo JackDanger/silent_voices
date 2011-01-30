3982429f9416345cf523be3ac636e6d31dc77158
Jack Danger Canty <gitcommit@6brand.com>
2011-01-30 15:59:23 -0800

    compiling via HAML

--- lib/silent_voices.rb
+++ lib/silent_voices.rb
@@ -1,4 +1,5 @@
 require 'feminizer'
+require 'haml'
 require File.expand_path File.join(File.dirname(__FILE__), 'silent_voices', 'gender')
 require File.expand_path File.join(File.dirname(__FILE__), 'silent_voices', 'page')
 require File.expand_path File.join(File.dirname(__FILE__), 'silent_voices', 'compiler')
--- lib/silent_voices/page.rb
+++ lib/silent_voices/page.rb
@@ -22,39 +22,7 @@ module SilentVoices
     end
 
     def write
-      File.open(outfile, 'w') {|f| f.write html }
-    end
-
-    def template
-      stylesheet_path = is_a?(StartPage) ? 'style.css' : '../style.css'
-      h = []
-      nav = "<div class='nav'>" +
-            "<div class='prev_link'>#{prev_link}</div>" +
-            "<div class='home_link'><a href='#{SilentVoices.start_page.path_from(self)}'>Silent Voices</a></div>" +
-            "<div class='next_link'>#{next_link}</div>" +
-            "</div>"
-
-      h << "<html><head>"
-      h << "<link href='#{stylesheet_path}' media='screen' rel='stylesheet' type='text/css' />"
-      h << "<link href='http://fonts.googleapis.com/css?family=Bentham' rel='stylesheet' type='text/css'>"
-      h << "<link href='http://fonts.googleapis.com/css?family=Neuton' rel='stylesheet' type='text/css'>"
-      h << "</head><body>"
-      h << "<div class='container'>"
-      h << nav unless is_a?(StartPage)
-      yield h
-      h << nav unless is_a?(StartPage)
-      h << "</div>"
-      h << "<div class='footer'>Created by <a href='http://jackcanty.com'>Jack</a>, send corrections and feedback to <em>silentvoices at jackcanty dot com</em></div>"
-      h << "</body></html>"
-      h.join("\n")
-    end
-
-    def prev_link
-      "<a href='#{prev_page.path_from(self)}'>&laquo; #{prev_page.name}</a>" if prev_page
-    end
-
-    def next_link
-      "<a href='#{next_page.path_from(self)}'>#{next_page.name} &raquo;</a>" if next_page
+      File.open(outfile, 'w') {|f| f.write render }
     end
 
     def decrement_string string
@@ -66,14 +34,14 @@ module SilentVoices
       string.succ
     end
 
-    def facebook_like
-      %Q{<div id="facebook_like"><iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fsilentvoicesbible.com/#{path_from(SilentVoices.start_page)}&amp;layout=button_count&amp;show_faces=false&amp;width=50&amp;action=recommend&amp;font=lucida+grande&amp;colorscheme=light&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:35px;" allowTransparency="true"></iframe></div>}
+    def render
+      Haml::Engine.new(File.read(ViewsDirectory + '/template.haml'), :filename => 'template').render(self) {
+        Haml::Engine.new(File.read(ViewsDirectory + '/' + view_file), :filename => view_file).render(self)
+      }
     end
 
-    def render
-      Haml::Engine.new(File.read(ViewsDirectory + '/template.haml')).render(self) {
-        Haml::Engine.new(File.read(ViewsDirectory + view_file)).render(self)
-      }
+    def stylesheet_path
+      '../style.css'
     end
 
     def self.write_all
@@ -107,24 +75,12 @@ module SilentVoices
       @book_pages ||= SilentVoices.pages.select {|page| BookPage === page }.sort_by(&:number)
     end
 
-    def html
-      template do |h|
-        h << '<h1>Silent Voices</h1>'
-        h << "<h3 class='quote'>\"If God is male then male is God.\" -- Mary Daly</h3>"
-        h << "<div class='entries books'>"
-        book_pages.each do |book|
-          h << "<div class='entry book'>"
-          h << "<div class='anchor'>#{book.number}</div>"
-          h << "<div class='content'><a href='#{book.path_from(self)}'>#{book.name}</a></div>"
-          h << "</div>"
-        end
-        h << facebook_like
-        h << "</div>"
-      end
-    end
-
     def path_from(page)
       "../index.html"
+    end
+
+    def stylesheet_path
+      'style.css'
     end
 
     def filename
@@ -163,21 +119,6 @@ module SilentVoices
 
     def chapter_pages
       @chapter_pages ||= SilentVoices.pages.select {|page| ChapterPage === page && book == page.book }.sort_by(&:number)
-    end
-
-    def html
-      template do |h|
-        h << "<h2 class='book'>#{name}</h2>"
-        h << "<div class='entries chapters'>"
-        chapter_pages.each do |chapter|
-          h << "<div class='entry chapter'>"
-          h << "<div class='anchor'>Chapter #{chapter.number.to_i}</div>"
-          h << "<div class='content chapter_preview'><a href='#{chapter.path_from(self)}'>#{chapter.verses.first[:text].to_s[0, 45]}[...]</a></div>"
-          h << "</div>"
-        end
-        h << facebook_like
-        h << "</div"
-      end
     end
 
     def name
@@ -234,22 +175,6 @@ module SilentVoices
       @next_page ||=
         SilentVoices.pages.detect {|page| name(increment_string(number)) == page.name } ||
         SilentVoices.pages.detect {|page| page.is_a?(BookPage) && increment_string(book[:number]).eql?(page.number) }
-    end
-
-    def html
-      template do |h|
-        h << "<h2 class='chapter'><a href='#{book_page.path_from(self)}'>#{@book[:name]}</a> #{number}</h2>"
-        h << "<div class='entries chapter'>"
-        verses.each do |verse|
-          h << "<div class='entry verse'>"
-          h << "<div class='anchor number'><a name='#{verse[:number]}' href='##{verse[:number]}'>#{verse[:number]}</a></div>"
-          h << "<div class='content text'>#{verse[:text]}</div>"
-          h << "</div>"
-        end
-        h << "</div>"
-        h << facebook_like
-        h << "<div class='back_to_book'><a href='#{book_page.path_from(self)}'>#{book_page.name}</a></div>"
-      end
     end
 
     def name(num = number)
