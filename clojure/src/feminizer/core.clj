8938914f566d924aad9165c583620841a1368f11
Jack Danger <email@jackcanty.com>
2012-08-21 19:55:58 -0700

    Got actual loose/tight matching working.
    Tests pass.

--- clojure/src/feminizer/core.clj
+++ clojure/src/feminizer/core.clj
@@ -1,29 +1,71 @@
 (ns feminizer.core
-  (:use [clojure.string :only [split join replace]])
+  (:use [clojure.string :only [split join]])
   (:use clojure.test))
 
-(defn pairs [file]
-  (map #(split % #",") (split (slurp file) #"\n")))
+(def -replace clojure.string/replace) ; how to do this with :use without warnings?
 
-(def default-forms (pairs "src/feminizer/default.forms"))
-(def default-form-map
-  (reduce (fn [map pair]
-            (assoc map (first pair) (last  pair)
-                       (last  pair) (first pair)))
-          {}
-          default-forms))
+(def word-boundary "[ ,.;'\"(){}]")
+(def loose-regex (ref #""))
+(def tight-regex (ref #""))
+(def forms (ref {}))
 
-(def form-map default-form-map)
+(defn set-regexes []
+  (ref-set loose-regex
+           (re-pattern (join "|"
+                             (map #(str word-boundary % word-boundary)
+                                  (keys @forms)))))
+  (ref-set tight-regex
+           (re-pattern (join "|"
+                             (map #(str "(" % ")")
+                                  (keys @forms))))))
 
-(def feminize-regexp
-  (re-pattern (join "|" (keys form-map))))
+(defn each-pair [fn pairs]
+  (dosync (loop [[male female & more] pairs]
+            (alter forms fn male female female male)
+            (if more
+              (recur more)))
+          (set-regexes)))
 
+(defn learn [& pairs]
+  (each-pair assoc pairs))
+
+(defn forget [& pairs]
+  (each-pair dissoc pairs))
+
+
+; add defaults
+(apply learn (flatten (map #(split % #",")
+                            (split (slurp "src/feminizer/default.forms") #"\n"))))
 
 (defn feminize [text]
-  (replace text feminize-regexp form-map))
+  (-replace
+    (-replace (str " " text " ") ; normalize string initial/final terms
+              @loose-regex
+              (fn [match]
+                (-replace match
+                          @tight-regex
+                          (fn [[exact-match & _]]
+                            (@forms exact-match)))))
+    #" (.*) "
+    (fn [[_ s]] s))) ; strip normalized whitespace
 
-(testing "feminize"
-  (testing "with default forms"
-    (is (= (feminize "a man, (king) is not a woman or a queen")
-                     "a woman, (queen) is not a man or a king"))))
+
+(deftest t
+  (testing "feminize"
+    (testing "with default forms"
+      (is (= (feminize "man, (king) is not a woman or a queen.")
+                       "woman, (queen) is not a man or a king."))))
+  (testing "learn"
+    (learn "ovaries" "testes")
+    (is (= "she's got ovaries" (feminize "he's got testes")))
+    (is (= "she's got ovaries" (feminize (feminize "she's got ovaries")))))
+
+  (testing "forget"
+    (is (=           "this lady is crazy for that gentleman"
+           (feminize "this gentleman is crazy for that lady")))
+    (forget "lady" "gentleman")
+    (is (=           "this lady is crazy for that gentleman"
+           (feminize "this lady is crazy for that gentleman"))))
+
+)
 
