a56a56edc7abeb667a240bdff43a05fbc09db87d
Jack Danger <email@jackcanty.com>
2012-08-21 19:55:57 -0700

    let's get dangerous

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,4 @@
 (defproject silentvoicesbible "0.0.1"
   :description "Clojure port of the silentvoicesbible.com compiler"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0-alpha4"]]
   :main silentvoicesbible.core)
