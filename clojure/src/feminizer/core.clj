d095d1aa7f5da8c7d7fc4dd4822ab7c0819d395d
Jack Danger <email@jackcanty.com>
2012-08-21 19:55:55 -0700

    testing feminizer

--- clojure/src/feminizer/core.clj
+++ clojure/src/feminizer/core.clj
@@ -1,5 +1,6 @@
 (ns feminizer.core
-  (:use [clojure.string :only [split join replace]]))
+  (:use [clojure.string :only [split join replace]])
+  (:use clojure.test))
 
 (defn pairs [file]
   (map #(split % #",") (split (slurp file) #"\n")))
@@ -8,9 +9,9 @@
 (def default-form-map
   (reduce (fn [map pair]
             (assoc map (first pair) (last  pair)
-                       (last  pair) (first pair))) {} default-forms))
-
-(println (default-form-map "man"))
+                       (last  pair) (first pair)))
+          {}
+          default-forms))
 
 (def form-map default-form-map)
 
@@ -21,4 +22,8 @@
 (defn feminize [text]
   (replace text feminize-regexp form-map))
 
-(println (feminize "a man, (king) is not a woman or a queen"))
+(testing "feminize"
+  (testing "with default forms"
+    (is (= (feminize "a man, (king) is not a woman or a queen")
+                     "a woman, (queen) is not a man or a king"))))
+
