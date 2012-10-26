4585127ba8fd90faa1d6e0c572c37f97393e791b
Jack Danger <jackdanger@squareup.com>
2012-10-26 14:56:21 -0700

    typo

--- clojure/spec/feminizer/core_spec.clj
+++ clojure/spec/feminizer/core_spec.clj
@@ -14,11 +14,17 @@
              (feminize "testes, not ovaries"))
     ; check the round-trip
     (should= "ovaries, not testes" (feminize (feminize "ovaries, not testes"))))
+
   (it "translates forms even when they're adjacent"
     (learn "king" "queen")
     (learn "son"  "daughter")
     (should=           "queen daughter"
-             (feminize "king son"))))
+             (feminize "king son")))
+
+  (it "doesn't replace word-fragments"
+    (learn "male" "female")
+    (should=           "Tamale for the female, please."
+             (feminize "Tamale for the male, please."))))
 
 (describe "forget"
   (it "removes from the known forms"
@@ -32,7 +38,7 @@
              (feminize "this lady and that lady are crazy for that gentleman"))))
 (describe "whitespace"
   (it "is ignored"
-    (should=        "   3 spaces preceed and two newlines follow\n\n"
-          (feminize "   3 spaces preceed and two newlines follow\n\n"))))
+    (should=        "   3 spaces precede and two newlines follow\n\n"
+          (feminize "   3 spaces precede and two newlines follow\n\n"))))
 
 
