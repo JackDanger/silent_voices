d38df90f0de256f9c6086f21e6ca4f4af2c22430
Jack Danger <jackdanger@squareup.com>
2012-10-30 01:06:10 -0700

    chapter-specific templates
    Currently seeing a moustache OoMemory error

--- clojure/src/silentvoicesbible/html.clj
+++ clojure/src/silentvoicesbible/html.clj
@@ -5,43 +5,39 @@
 
 (def output-directory "_site")
 
-(defn- filename [book]
+(defn- filename [book chapter-num]
   (str (clojure.string/replace (.toLowerCase (:name book))
                                #"[^\d\w-_]"
-                               "-") ".html"))
+                               "-") "-" chapter-num ".html"))
 
-(defn- write-to [path lines]
+(defn- write-to [path s]
   (print ".")
   (.mkdir (java.io.File. output-directory))
-  (with-open [writer (clojure.java.io/writer path)]
-    (doseq [line lines] (.write writer line))))
+  (with-open [writer (clojure.java.io/writer path)] (.write writer s)))
 
 (defn- index []
   (write-to
     (str output-directory "/index.html")
-    (concat ["<h1>Silent Voices Tanakh</h1>"]
-            ["<ul>"]
-            (map #(str "<li><a href=" (filename %1) ">"
-                       (:name %1)
-                       "</a></li>")
-                 tanakh)
-            ["<ul>"])))
+      (render-resource "templates/index.mustache"
+                       {:books (map #(assoc % :filename (filename % 1)) tanakh)})))
 
-(defn- book-str [book translation medium]
-  (render-resource "templates/index.mustache"
-                   {:title (:name book)
-                    :cs [{:verses [{:html "<>"}]}]
+(defn- page [book chapter translation]
+  (render-resource "templates/chapter.mustache"
+                   {:book book
+                    :chapter (:number chapter)
+                    :chapters (map (fn [c] {:filename (filename book c) :number (:number c)}) (:chapters book))
                     :columns
-                      (let [vs (for [v (.verses book)] (assoc v :html (.html v translation)))]
-                        (for [cols (partition (/ (count vs) 2) vs)] {:verses cols }))
-                    :current-page (filename book)}))
+                      (let [vs (for [v (.verses chapter)] (assoc v :html (.html v translation)))]
+                        (for [cols (partition (int (/ (count vs) 2)) vs)] {:verses cols}))
+                    :current-page (filename book (:number chapter))}))
 
 (defn generate [args]
   (time
     (let [translation (str (or (nfirst (mapcat #(re-seq #"--translation=([\w]+)" %) args))
                                 "feminized"))]
-      (.exec (Runtime/getRuntime) "ln -fs ../resources/assets _site/assets")
+      (.exec (Runtime/getRuntime) "mkdir -p _site;ln -fs ../resources/assets _site/assets")
       (index)
-      (doseq [book tanakh]
-        (write-to (str output-directory "/" (filename book))
-                  [(book-str book translation "html")])))))
+      (let [book (first tanakh)]
+      (doseq [chapter (:chapters book)]
+        (write-to (str output-directory "/" (filename book (:number chapter)))
+                  (page book chapter translation)))))))
