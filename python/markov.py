f08e0d7882655fda74694a33ba8eee84f0f6427c
Jack Danger Canty <github@jackcanty.com>
2017-06-04 17:28:40 -0700

    DUNE

--- python/markov.py
+++ python/markov.py
@@ -3,12 +3,15 @@ import re
 
 bible = "../8924.txt.utf8"
 declaration = "../declaration_of_independence.txt"
-with open(bible) as f:
+dune = "dune.txt"
+with open(dune) as f:
     text = f.read()
 
 text = re.sub(r'\d{3}:\d{3} ', ' ', text)
 
 text_model = markovify.Text(text)
 
-for i in range(55):
+for i in range(1000):
     print(text_model.make_short_sentence(140))
+    print(text_model.make_short_sentence(60))
+    print(text_model.make_short_sentence(30))
